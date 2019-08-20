af17786086e29e72f67448c649cd3f016beefbcc
AytoLekunberri <consullekunberri@gmail.com>
2019-08-20 11:11:45 +0200

    Prueba censo

--- lib/census_caller.rb
+++ lib/census_caller.rb
@@ -4,7 +4,7 @@ class CensusCaller
     if Setting["feature.remote_census"].present?
       response = RemoteCensusApi.new.call(document_type, document_number, date_of_birth, postal_code)
     else
-      #response = CensusApi.new.call(document_type, document_number)
+      response = CensusApi.new.call(document_type, document_number)
     end
     response = LocalCensus.new.call(document_type, document_number) unless response.valid?
 
