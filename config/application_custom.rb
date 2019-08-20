746cc45b84ff034b2b5c45734e20a790b0239161
AytoLekunberri <consullekunberri@gmail.com>
2019-08-20 08:45:54 +0200

    Prueba con idiomas 1

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,6 +1,6 @@
 module Consul
   class Application < Rails::Application
 	config.i18n.default_locale= :es
-	config.i18n.available_locales = [ :es, :eu-ES, :en ]
+	config.i18n.available_locales = [ :es, :eu, :en ]
   end
 end
