1bb9cdfe810ac5c146902a500c76518cabd4c593
AytoLekunberri <consullekunberri@gmail.com>
2019-08-23 08:37:33 +0200

    Despliegue verificacion usuarios

--- lib/census_caller.rb
+++ lib/census_caller.rb
@@ -3,24 +3,21 @@ class CensusCaller
   def call(document_type, document_number, date_of_birth, postal_code)
   
   tipo = ''
-	if document_type == "1"
+	case document_type
+	when '1'
 		tipo = 'DNI'
-		#log.debug("ES 1")
+	when '2'
+		tipo = 'Pasaporte'
+	when '3'
+		tipo = 'Tarjeta de residencia'
 	end
-    if Setting["feature.remote_census"].present?
-      response = RemoteCensusApi.new.call(tipo, document_number, date_of_birth, postal_code)
-    else
-      response = CensusApi.new.call(tipo, document_number)
-    end
-
-	#require 'logger'
-	#log = Logger.new('log.txt', 'daily')
-	#log.debug("MOSTRANDO DATOS")
-	#log.debug(document_number)
-	#log.debug(document_type)
+    #if Setting["feature.remote_census"].present?
+     # response = RemoteCensusApi.new.call(tipo, document_number, date_of_birth, postal_code)
+    #else
+     # response = CensusApi.new.call(tipo, document_number)
+    #end
 	
-	#log.debug(tipo)
-    response = LocalCensus.new.call(tipo, document_number) unless response.valid?
+    response = LocalCensus.new.call(tipo, document_number) #unless response.valid?
 
     response
   end
