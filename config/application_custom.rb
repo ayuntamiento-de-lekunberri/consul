fd33305778a2c554df2ef1f15d2f4850cba1b132
AytoLekunberri <consullekunberri@gmail.com>
2019-09-10 09:57:56 +0200

    Cambio en idioma. Euskera lengua por defecto

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,6 +1,6 @@
 module Consul
   class Application < Rails::Application
-	config.i18n.default_locale= :eu
+	config.i18n.default_locale= :es
 	config.i18n.available_locales = [ :es, :eu ]
   end
 end
